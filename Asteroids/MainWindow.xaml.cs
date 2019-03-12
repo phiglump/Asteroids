609c3e70955618e22f7987b2ec519a5a54b7e426
OppositeIllusion <45372082+OppositeIllusion@users.noreply.github.com>
2019-03-12 15:57:47 -0500

    WIP

--- Asteroids/MainWindow.xaml.cs
+++ Asteroids/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Asteroids
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
