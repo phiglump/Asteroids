cd6a7d2fa47d050889aaf3f5a4c4dd7b7fb20d6e
OppositeIllusion <45372082+OppositeIllusion@users.noreply.github.com>
2019-03-20 21:23:19 -0500

    Working Database

--- Asteroids/HighScoreScreen.xaml.cs
+++ Asteroids/HighScoreScreen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,21 @@ namespace Asteroids
     /// </summary>
     public partial class HighScoreScreen : Page
     {
+        OleDbConnection cn;
         public HighScoreScreen()
         {
             InitializeComponent();
+            cn = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = |DataDirectory|Highscores.accdb");
+            cn.Open();
+            string Initials = "select * from Highscores";
+            OleDbCommand cmd = new OleDbCommand(Initials, cn);
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += "" + read[0].ToString() + ".            " + read[2].ToString() + "          " + read[1].ToString() + "\n";
+            }
+            Text1.Text = data;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
